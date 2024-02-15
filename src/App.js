bd97af4fb9b98aac8c71e6525be63aee64c719fb
Nicholas Chan <99701277+nicknahc@users.noreply.github.com>
2024-02-15 14:12:18 -0800

    added work section oops

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.scss";
 import Home from "./pages/Home.js";
 import AboutMe from "./pages/AboutMe.js";
+import Work from "./pages/Work.js";
 function App() {
   return (
     <div className="App">
       <Home />
+      <Work />
       <AboutMe />
     </div>
   );
---
+++ src/pages/Work.js
@@ -0,0 +1,23 @@
+import "../styles/Work.scss";
+function Work() {
+  const menu = document.getElementById("work");
+  Array.from(document.getElementsByClassName("work-menu-item")).forEach(
+    (item, index) => {
+      item.onmouseover = () => {
+        menu.dataset.activeIndex = index;
+      };
+    }
+  );
+  return (
+    <div id="work" className="work-container bg-neutral-900 text-accent-400">
+      <ul className="" id="work-menu">
+        <li className="work-menu-item fs-700">This is my list ONE</li>
+        <li className="work-menu-item fs-700">LIST TWO</li>
+        <li className="work-menu-item fs-700">THIS IS MY LIST 3</li>
+        <li className="work-menu-item fs-700">FOUR</li>
+      </ul>
+      <div className="work-menu-background"></div>
+    </div>
+  );
+}
+export default Work;
