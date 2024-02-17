1a6d597fbcb7174b6e144b11f1ae036f0044ef12
Nicholas Chan <99701277+nicknahc@users.noreply.github.com>
2024-02-17 00:21:51 -0800

    simplified some css

--- src/pages/Work.js
+++ src/pages/Work.js
@@ -7,27 +7,34 @@ function Work() {
         <li id="item-1" className="work-menu-item fs-700">
           Work Experience
         </li>
+        <div className="work-example">
+          <a
+            className="gymbuddy-link"
+            target="_blank"
+            rel="noreferrer"
+            href="https://getgymbuddy.com"
+          >
+            <img
+              src={gymbuddydemo}
+              height="auto"
+              width="800"
+              alt="A demonstration gif of the gymbuddy website"
+            ></img>
+          </a>
+          <a
+            className="repo-link"
+            target="_blank"
+            rel="noreferrer"
+            href="https://github.com/fingit123/gymbuddyweb"
+          >
+            <div>GITHUB</div>
+          </a>
+        </div>
         <li className="work-menu-item fs-700">Projects</li>
         {/* <li className="work-menu-item fs-700">THIS IS MY LIST 3</li>
         <li className="work-menu-item fs-700">FOUR</li> */}
       </ul>
       <div className="work-menu-background"></div>
-      <div className="work-example">
-        <img
-          src={gymbuddydemo}
-          height="auto"
-          width="600"
-          alt="A demonstration gif of the gymbuddy website"
-        ></img>
-        <a
-          className="gymbuddy-link"
-          target="_blank"
-          rel="noreferrer"
-          href="https://getgymbuddy.com"
-        >
-          <div>LIVE SITE</div>
-        </a>
-      </div>
     </div>
   );
 }
